29f462bc3eb36741067b5c5593839e4ea5eb8531
Milled <ZhengJaho@gmail.com>
2017-02-26 01:15:18 +0800

    实验一

---
+++ 136.cpp
@@ -0,0 +1,7 @@
+#include "stdio.h"
+int main(int argc, char const *argv[]) {
+  char str1[]="What a beautiful campus!";
+  char str2[]="I wash you every success!";
+  printf("%s%s\n",str1,str2);
+  return 0;
+}
